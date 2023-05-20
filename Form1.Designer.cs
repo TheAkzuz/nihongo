732000fa9fdc13515d62d194fb626b56ce9e4cb9
akzuz <akzuz@AKZUZ>
2023-05-20 23:27:21 +0300

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,370 @@
+﻿namespace nihongo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.listView2 = new System.Windows.Forms.ListView();
+            this.button3 = new System.Windows.Forms.Button();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.listView3 = new System.Windows.Forms.ListView();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.checkBox4 = new System.Windows.Forms.CheckBox();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.checkBox5 = new System.Windows.Forms.CheckBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(48, 278);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(243, 72);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "сгенерировать иероглиф";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(703, 306);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(243, 71);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "проверить правильность";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // listView1
+            // 
+            this.listView1.Font = new System.Drawing.Font("Microsoft Sans Serif", 35F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(48, 57);
+            this.listView1.Name = "listView1";
+            this.listView1.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.listView1.Size = new System.Drawing.Size(243, 192);
+            this.listView1.TabIndex = 2;
+            this.listView1.TileSize = new System.Drawing.Size(15, 15);
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.List;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(706, 226);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(243, 56);
+            this.comboBox1.TabIndex = 3;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // listView2
+            // 
+            this.listView2.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.listView2.HideSelection = false;
+            this.listView2.Location = new System.Drawing.Point(706, 57);
+            this.listView2.Name = "listView2";
+            this.listView2.Size = new System.Drawing.Size(225, 97);
+            this.listView2.TabIndex = 4;
+            this.listView2.UseCompatibleStateImageBehavior = false;
+            this.listView2.View = System.Windows.Forms.View.List;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(396, 336);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(176, 38);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "подключить библиотеку";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(396, 306);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(176, 24);
+            this.comboBox2.TabIndex = 6;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(393, 377);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 16);
+            this.label1.TabIndex = 7;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(57, 35);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(178, 16);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "тут выводятся иероглифы";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(703, 35);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(227, 16);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "тут пишется ошибся ли ты или нет";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(383, 278);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(201, 16);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "тут надо написать имя файла";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(703, 194);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(154, 16);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "тут надо писать слово";
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(24, 392);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(143, 20);
+            this.checkBox2.TabIndex = 14;
+            this.checkBox2.Text = " Режим челенджа";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            this.checkBox2.CheckedChanged += new System.EventHandler(this.checkBox2_CheckedChanged);
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Location = new System.Drawing.Point(24, 418);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(138, 20);
+            this.checkBox3.TabIndex = 15;
+            this.checkBox3.Text = "обратный режим";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            this.checkBox3.CheckedChanged += new System.EventHandler(this.checkBox3_CheckedChanged);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(24, 364);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(108, 20);
+            this.checkBox1.TabIndex = 16;
+            this.checkBox1.Text = "Debug Mode";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // listView3
+            // 
+            this.listView3.HideSelection = false;
+            this.listView3.Location = new System.Drawing.Point(737, 407);
+            this.listView3.Name = "listView3";
+            this.listView3.Size = new System.Drawing.Size(174, 65);
+            this.listView3.TabIndex = 17;
+            this.listView3.UseCompatibleStateImageBehavior = false;
+            this.listView3.Visible = false;
+            this.listView3.SelectedIndexChanged += new System.EventHandler(this.listView3_SelectedIndexChanged);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(703, 157);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(44, 16);
+            this.label6.TabIndex = 18;
+            this.label6.Text = "label6";
+            this.label6.Visible = false;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(48, 255);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(44, 16);
+            this.label7.TabIndex = 19;
+            this.label7.Text = "label7";
+            this.label7.Visible = false;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 22F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label8.Location = new System.Drawing.Point(309, 157);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(99, 42);
+            this.label8.TabIndex = 20;
+            this.label8.Text = "www";
+            this.label8.Visible = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(423, 57);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(101, 76);
+            this.pictureBox1.TabIndex = 21;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(972, 567);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(44, 16);
+            this.label9.TabIndex = 22;
+            this.label9.Text = "v:1.3.1";
+            // 
+            // checkBox4
+            // 
+            this.checkBox4.AutoSize = true;
+            this.checkBox4.Location = new System.Drawing.Point(24, 444);
+            this.checkBox4.Name = "checkBox4";
+            this.checkBox4.Size = new System.Drawing.Size(115, 20);
+            this.checkBox4.TabIndex = 23;
+            this.checkBox4.Text = "ограничение";
+            this.checkBox4.UseVisualStyleBackColor = true;
+            this.checkBox4.CheckedChanged += new System.EventHandler(this.checkBox4_CheckedChanged);
+            this.checkBox4.Click += new System.EventHandler(this.checkBox4_CheckedChanged);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(145, 442);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(48, 22);
+            this.numericUpDown1.TabIndex = 24;
+            this.numericUpDown1.Visible = false;
+            this.numericUpDown1.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            // 
+            // checkBox5
+            // 
+            this.checkBox5.AutoSize = true;
+            this.checkBox5.Location = new System.Drawing.Point(24, 471);
+            this.checkBox5.Name = "checkBox5";
+            this.checkBox5.Size = new System.Drawing.Size(117, 20);
+            this.checkBox5.TabIndex = 25;
+            this.checkBox5.Text = "показ ответа";
+            this.checkBox5.UseVisualStyleBackColor = true;
+            this.checkBox5.CheckedChanged += new System.EventHandler(this.checkBox5_CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Info;
+            this.ClientSize = new System.Drawing.Size(1028, 592);
+            this.Controls.Add(this.checkBox5);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.checkBox4);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.listView3);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.checkBox3);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.listView2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "日本語";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ListView listView2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.ListView listView3;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.CheckBox checkBox4;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.CheckBox checkBox5;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,272 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Diagnostics.Eventing.Reader;
+
+namespace nihongo
+{
+    public partial class Form1 : Form
+    {
+
+        
+
+        string line;
+
+        int zop = 0;
+        int lon = 0;
+        int lim = 0;
+        int r;
+        string[,] glag = new string[256,3];
+        Random random = new Random();
+        int mode = 0;
+        int hp = 3;
+        int xp = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            numericUpDown1.Value = 2;
+            numericUpDown1.Minimum = 2;
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            listView1.Items.Clear();
+            
+            label1.Visible = false;
+      
+
+           // numericUpDown1.Maximum = zop;
+           
+            while (r == lim )
+            {
+                r = random.Next(0, zop);
+            }
+            lim = r;
+            string vie = glag[r,0];
+            listView1.Items.Add(vie);
+            listView2.Items.Clear();
+            label8.Text = "правильный ответ:\n" + glag[r, 1];
+        }
+
+        string lol;
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            listView2.Items.Clear();
+
+            lol = comboBox1.Text;
+
+            if(lol == glag[r,1])
+            {
+                listView2.Items.Add("Верно");
+
+                if (mode == 1)
+                {
+                    xp++;
+                }
+            }
+            else
+            {
+                if (mode == 1)
+                {
+                    hp--;
+
+                }
+                listView2.Items.Add("Неверно");
+            }
+            label8.Text = "правильный ответ:\n"+ glag[r, 1];
+
+            //listView2.Items.Add(lol);
+            label6.Text = "Ваша жизнь = " + hp;
+            label7.Text = "Правльных угаданых иероглифоф =" + xp;
+
+            if (hp==0)
+            {
+                label8.Visible = true;
+                mode = 0;
+                label8.Text = "Жизнь кончилась.\nВаш счёт = " + xp + "\nперезапустите режим\n челенджа для повтора";
+            }
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                StreamReader sr = new StreamReader(comboBox2.Text + ".txt");
+                listView1.Items.Clear();
+                label1.Visible = true;
+                label1.Text = "библиотека загружена";
+                lon = 0;
+            line = sr.ReadLine();
+            int i = 0;
+            while (line != null)
+            {
+                    lon++;
+                string u = "";
+                string y = "";
+                    
+                    int k = 0;
+                    for (int a = 0; a < line.Length; a++)
+                {
+                   
+                   
+                    if (line[a] != '-' && line[a] != 'ー' && k ==0)
+                    {
+                        y = y + line[a];
+                        // listView2.Items.Add(y);
+                    }
+                        if (line[a] == '-' || line[a] == 'ー')
+                        {
+                            k = 1;
+                        }
+                    if (line[a] != '-' && line[a] != 'ー' && k == 1)
+                    {
+                        u = u + line[a];
+                            //listView2.Items.Add(Convert.ToString(line[a]));
+                    }
+                }
+                    //listView2.Items.Add(u);
+                    zop = lon;
+                    glag[i, 0] = y;
+                glag[i, 1] = u;
+                i++;
+
+                line = sr.ReadLine();
+            }
+
+        }
+                        catch (System.IO.FileNotFoundException)
+            {
+                label1.Text = "файл не обнаружен";
+
+            }
+            while (glag[zop, 0] != null)
+            {
+                zop++;
+            }
+            numericUpDown1.Maximum = zop;
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked == true)
+            {
+                listView3.Visible=true;
+            }
+            else
+            {
+                listView3.Visible = false;
+            }
+        }
+
+        private void checkBox2_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox2.Checked == true)
+            {
+                label6.Visible = true;
+                label7.Visible = true;
+                mode = 1;
+                xp = 0;
+                hp = 3;
+                label6.Text = "Ваша жизнь = " + hp;
+                label7.Text = "Правльных угаданых иероглифоф =" + xp;
+            }
+            else
+            {
+                label8.Visible = false;
+                label6.Visible = false;
+                label7.Visible = false;
+            }
+        }
+
+        private void listView3_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkBox3_CheckedChanged(object sender, EventArgs e)
+        {
+            string kan = "";
+            if (checkBox3.Checked==true)
+            {
+                
+                for(int i = 0; i<lon; i++)
+                {
+                    kan = glag[i, 0];
+                    glag[i, 0] = glag[i, 1];
+                    glag[i, 1] = kan;
+
+                }
+
+            }
+            else
+            {
+                for (int i = 0; i < lon; i++)
+                {
+                    kan = glag[i, 1];
+                    glag[i, 1] = glag[i, 0];
+                    glag[i, 0] = kan;
+                }
+            }
+
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+
+            zop = Convert.ToInt32( numericUpDown1.Value);
+        }
+
+        private void checkBox4_CheckedChanged(object sender, EventArgs e)
+        {
+            if(checkBox4.Checked==true)
+            {
+                numericUpDown1.Visible = true;
+                zop = Convert.ToInt32( numericUpDown1.Value);
+
+            }
+            if(checkBox4.Checked==false)
+            {
+                numericUpDown1.Visible = false;
+                zop = lon-1;
+            }
+
+        }
+
+        private void checkBox5_CheckedChanged(object sender, EventArgs e)
+        {
+
+            if(checkBox5.Checked==true)
+            {
+                label8.Visible = true;
+            }
+            if(checkBox5.Checked == false)
+            {
+                label8.Visible = false;
+
+            }
+
+        }
+    }
+}
